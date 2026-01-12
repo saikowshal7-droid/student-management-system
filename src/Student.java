e825a073a051eeb3a73c10b9f7fd63ddd90144c0
saikowshal7-droid <saikowshal7@gmail.com>
2026-01-12 19:54:30 +0530

    Create Student.java

---
+++ src/Student.java
@@ -0,0 +1,19 @@
+import java.io.*;
+public class Student {
+    int id;
+    String name;
+    int age;
+
+    Student(int id, String name, int age) {
+        this.id = id;
+        this.name = name;
+        this.age = age;
+     }
+    public void displayStudent() {
+        System.out.println("ID: " + id);
+        System.out.println("Name: " + name);
+        System.out.println("Age: " + age);
+}
+}
+
+    
